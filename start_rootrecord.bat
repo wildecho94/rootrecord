346b9fdb1aad53ca90043bc76e7ec6e3e5a2e75b
Wild Echo <wildecho94@gmail.com>
2026-01-11 20:53:48 +0000

    Auto update: include recent log + changes

--- start_rootrecord.bat
+++ start_rootrecord.bat
@@ -7,9 +7,6 @@ echo.
 
 cd /d "C:\Users\Alexrs94\Desktop\programfiles\rootrecord"
 
-echo [%date% %time%] Cleaning old log...
-if exist "..\debug_rootrecord.log" del "..\debug_rootrecord.log"
-
 echo [%date% %time%] Running publish_rootrecord.py...
 python "C:\Users\Alexrs94\Desktop\programfiles\publish_rootrecord.py"
 
