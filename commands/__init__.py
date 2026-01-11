10e1dcb5d8a59f6075ba8129883a3dd0934f692b
Wild Echo <wildecho94@gmail.com>
2026-01-11 04:29:21 +0000

    Auto update: include recent log

---
+++ commands/__init__.py
@@ -0,0 +1,9 @@
+# commands/__init__.py
+# Edited Version: 1.42.20260110
+
+"""
+Makes the commands folder a proper Python package
+Required for relative imports in some plugin loader setups
+"""
+
+# empty file - presence alone is enough
