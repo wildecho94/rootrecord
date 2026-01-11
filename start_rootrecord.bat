43ba2f65bbdd990734f9c30ca4987e683eef47a4
Wild Echo <wildecho94@gmail.com>
2026-01-11 04:24:03 +0000

    Auto update: include recent log

--- start_rootrecord.bat
+++ start_rootrecord.bat
@@ -5,8 +5,10 @@ echo Starting Rootrecord system...
 echo Press Ctrl+C to stop
 echo.
 
+cd /d "%~dp0rootrecord"
+
 echo [%date% %time%] Running publish_rootrecord.py...
-python "C:\Users\Alexrs94\Desktop\programfiles\publish_rootrecord.py"
+python ..\publish_rootrecord.py
 
 echo [%date% %time%] Starting core.py ...
 echo.
