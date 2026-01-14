d1354c04c110a4445bd3d9e415be42337004ef04
Wild Echo <wildecho94@gmail.com>
2026-01-14 03:19:18 +0000

    Auto update: include recent log + changes

--- start_rootrecord.bat
+++ start_rootrecord.bat
@@ -10,13 +10,9 @@ cd /d "C:\Users\Alexrs94\Desktop\programfiles\rootrecord"
 echo [%date% %time%] Running publish_rootrecord.py...
 python "C:\Users\Alexrs94\Desktop\programfiles\publish_rootrecord.py"
 
-echo [%date% %time%] Starting Flask web server in new window...
-start "Flask Dashboard" cmd /k "cd /d C:\Users\Alexrs94\Desktop\programfiles\rootrecord\web && python app.py"
-timeout /t 5 >nul
-
-echo [%date% %time%] Starting Cloudflare Tunnel in new window...
-start "Cloudflare Tunnel" cmd /k "cd /d C:\Users\Alexrs94\Desktop\programfiles\rootrecord\web && cloudflared.exe tunnel --config cloudflared-config.yml run"
-timeout /t 8 >nul
+echo [%date% %time%] Starting Web Server + Cloudflare Tunnel in new window...
+start "Web + Tunnel" cmd /k "cd /d C:\Users\Alexrs94\Desktop\programfiles\rootrecord\web && python app.py & cloudflared.exe tunnel --config cloudflared-config.yml run"
+timeout /t 10 >nul
 
 echo [%date% %time%] Starting core.py ...
 echo.
